ec79818c52684e9fec6ba886e7a442594663cb7f
sabarirajpazhani <sabarirajpazhani@gmail.com>
2024-09-02 22:31:12 +0530

    Intinial Routing test

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,12 @@ const dotenv = require('dotenv');
 const path=require('path');
 dotenv.config({path: path.join(__dirname,'config','config.env')})
 
+const signUp = require('./routes/signUp')
+const login = require('./routes/login')
+
+app.use('/api/v1/',login);
+app.use('/api/v1/',signUp);
+
 app.listen (process.env.PORT,()=>{
     console.log(`Server Listening to Port ${process.env.PORT} in ${process.env.NODE_ENV}`)
 });
