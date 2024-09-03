e09aa05a3f5d79621e761e33314b466718f09aab
sabarirajpazhani <sabarirajpazhani@gmail.com>
2024-09-03 11:49:29 +0530

    API for Register

--- backend/controllers/singUpController.js
+++ backend/controllers/singUpController.js
@@ -1,10 +1,25 @@
 const registerModel = require('../models/register');
 
 exports.getSignUp=async(req,res,next)=>{
-    const registers= await registerModel.find({});
-    res.json({
-        success:true,
-        message:'Successfully SignUp',
-        registers
-    })
+    // const registers= await registerModel.find({});
+    try{
+        const {username, email, password} = req.body;
+        let user = await registerModel.findOne({email});
+        if(user){
+            return res.status(409).json({
+                message: "User already Exists"
+            })
+        }
+        user = new registerModel({username,email,password});
+        await user.save();
+
+        res.status(201).json({
+            message:"User Registered Successfully"
+        });
+    }
+    catch(error){
+        res.status(500).json({
+            message:"Internal Server Error"
+        });
+    }
 }
