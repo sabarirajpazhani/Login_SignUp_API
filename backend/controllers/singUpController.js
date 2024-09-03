91d6d519561960de5d16fef284b13b27c92bee7a
sabarirajpazhani <sabarirajpazhani@gmail.com>
2024-09-03 11:21:52 +0530

    JSON data SetUp

--- backend/controllers/singUpController.js
+++ backend/controllers/singUpController.js
@@ -1,6 +1,10 @@
-exports.getSignUp=(req,res,next)=>{
+const registerModel = require('../models/register');
+
+exports.getSignUp=async(req,res,next)=>{
+    const registers= await registerModel.find({});
     res.json({
         success:true,
-        message:'Successfully SignUp'
+        message:'Successfully SignUp',
+        registers
     })
 }
