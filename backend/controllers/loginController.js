69a4bc69371e1bd03f77f980cacade1b2aa06b33
sabarirajpazhani <sabarirajpazhani@gmail.com>
2024-09-03 14:06:42 +0530

    Login API

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,6 +1,30 @@
-exports.getLogin=(req,res,next)=>{
-    res.json({
-        success : true,
-        message : "Successfully Login"
-    })
+const registerModel = require('../models/register');
+
+exports.getLogin=async(req,res,next)=>{
+
+    try{
+        const {email,password}=req.body;
+        const user = await registerModel.findOne({email});
+        if(!user){
+            return res.status(400).json({
+                message:"User Not Exists. So Please SignUp"
+            })
+        }
+        if(password != user.password){
+            return res.status(400).json({
+                message: "Invalid Password"
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            message:"Login Successful!!"
+
+        })
+    }
+    catch(error){
+        res.status(500).json({
+            message:"Internal Server Error"
+        });
+    }
 }
